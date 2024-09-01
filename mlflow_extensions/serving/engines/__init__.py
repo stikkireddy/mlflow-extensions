0183f068d0ce74fc7295b821ba41519e5d57cf70
Sri Tikkireddy <sri.tikkireddy+github@databricks.com>
2024-09-01 09:14:28 -0400

    give sglang friendly imports

--- mlflow_extensions/serving/engines/__init__.py
+++ mlflow_extensions/serving/engines/__init__.py
@@ -1,2 +1,3 @@
 from mlflow_extensions.serving.engines.ollama_engine import OllamaEngineConfig, OllamaEngineProcess
+from mlflow_extensions.serving.engines.sglang_engine import SglangEngineConfig, SglangEngineProcess
 from mlflow_extensions.serving.engines.vllm_engine import VLLMEngineConfig, VLLMEngineProcess
