18a2a9430cbf6823b4fc7b0446fb8a089c4ffa74
stikkireddy <stikkireddy>
2024-09-06 21:43:35 +0000

    added sanity tests for sglang

--- mlflow_extensions/databricks/prebuilt/vision/sglang.py
+++ mlflow_extensions/databricks/prebuilt/vision/sglang.py
@@ -14,47 +14,47 @@ _llava_model_serving_configs_7b = ServingConfig(
     minimum_memory_in_gb=30,
 )
 
-_llava_model_serving_configs_34b = ServingConfig(
+_llava_model_serving_configs_70b = ServingConfig(
     # rough estimate for the engines this includes model weights + kv cache + overhead + intermediate states
-    minimum_memory_in_gb=70,
+    minimum_memory_in_gb=100,
 )
 
-# https://huggingface.co/liuhaotian/llava-v1.5-7b
-LLAVA_1_5_LLAMA_CONFIG = EzDeployConfig(
-    name="llava_1_5_llama_based",
-    engine_proc=_ENGINE,
-    engine_config=_ENGINE_CONFIG(
-        model="liuhaotian/llava-v1.5-7b",
-        tokenizer_path="llava-hf/llava-1.5-7b-hf",
-        chat_template_builtin_name="vicuna_v1.1",
-        verify_chat_template=False,  # it will use the built in chat template
-    ),
-    serving_config=_llava_model_serving_configs_7b,
-)
+# # https://huggingface.co/liuhaotian/llava-v1.5-7b
+# LLAVA_1_5_LLAMA_CONFIG = EzDeployConfig(
+#     name="llava_1_5_llama_based",
+#     engine_proc=_ENGINE,
+#     engine_config=_ENGINE_CONFIG(
+#         model="liuhaotian/llava-v1.5-7b",
+#         tokenizer_path="llava-hf/llava-1.5-7b-hf",
+#         chat_template_builtin_name="vicuna_v1.1",
+#         verify_chat_template=False,  # it will use the built in chat template
+#     ),
+#     serving_config=_llava_model_serving_configs_7b,
+# )
 
-# https://huggingface.co/liuhaotian/llava-v1.6-vicuna-7b
-LLAVA_1_6_VICUNA_CONFIG = EzDeployConfig(
-    name="llava_1_6_7b_vicuna_based",
-    engine_proc=_ENGINE,
-    engine_config=_ENGINE_CONFIG(
-        model="liuhaotian/llava-v1.6-vicuna-7b",
-        tokenizer_path="llava-hf/llava-1.5-7b-hf",
-        chat_template_builtin_name="vicuna_v1.1",
-        verify_chat_template=False,  # it will use the built in chat template
-    ),
-    serving_config=_llava_model_serving_configs_7b,
-)
+# # https://huggingface.co/liuhaotian/llava-v1.6-vicuna-7b
+# LLAVA_1_6_VICUNA_CONFIG = EzDeployConfig(
+#     name="llava_1_6_7b_vicuna_based",
+#     engine_proc=_ENGINE,
+#     engine_config=_ENGINE_CONFIG(
+#         model="liuhaotian/llava-v1.6-vicuna-7b",
+#         tokenizer_path="llava-hf/llava-1.5-7b-hf",
+#         chat_template_builtin_name="vicuna_v1.1",
+#         verify_chat_template=False,  # it will use the built in chat template
+#     ),
+#     serving_config=_llava_model_serving_configs_7b,
+# )
 
-# https://huggingface.co/liuhaotian/llava-v1.6-34b
-LLAVA_1_6_YI_34B_CONFIG = EzDeployConfig(
-    name="llava_1_6_34b_yi_based",
-    engine_proc=_ENGINE,
-    engine_config=_ENGINE_CONFIG(
-        model="liuhaotian/llava-v1.6-34b",
-        tokenizer_path="liuhaotian/llava-v1.6-34b-tokenizer",
-    ),
-    serving_config=_llava_model_serving_configs_7b,
-)
+# # https://huggingface.co/liuhaotian/llava-v1.6-34b
+# LLAVA_1_6_YI_34B_CONFIG = EzDeployConfig(
+#     name="llava_1_6_34b_yi_based",
+#     engine_proc=_ENGINE,
+#     engine_config=_ENGINE_CONFIG(
+#         model="liuhaotian/llava-v1.6-34b",
+#         tokenizer_path="liuhaotian/llava-v1.6-34b-tokenizer",
+#     ),
+#     serving_config=_llava_model_serving_configs_7b,
+# )
 
 # https://huggingface.co/lmms-lab/llama3-llava-next-8b
 LLAVA_NEXT_LLAMA3_8B_CONFIG = EzDeployConfig(
@@ -62,23 +62,61 @@ LLAVA_NEXT_LLAMA3_8B_CONFIG = EzDeployConfig(
     engine_proc=_ENGINE,
     engine_config=_ENGINE_CONFIG(
         model="lmms-lab/llama3-llava-next-8b",
-        tokenizer_path="lmms-lab/llama3-llava-next-8b-tokenizer",
+        chat_template_builtin_name="llava_llama_3",
     ),
     serving_config=_llava_model_serving_configs_7b,
 )
 
 
+# https://huggingface.co/lmms-lab/llava-next-72b
+LLAVA_NEXT_QWEN_1_5_72B_CONFIG = EzDeployConfig(
+    name="llava_next_qwen_1_5_72b_based",
+    engine_proc=_ENGINE,
+    engine_config=_ENGINE_CONFIG(
+        model="lmms-lab/llama3-llava-next-8b",
+        chat_template_builtin_name="chatml-llava",
+    ),
+    serving_config=_llava_model_serving_configs_70b,
+)
+
+# https://huggingface.co/lmms-lab/llava-onevision-qwen2-7b-ov
+LLAVA_ONEVISION_QWEN_2_7B_CONFIG = EzDeployConfig(
+    name="llava_onevision_qwen_2_7b_based",
+    engine_proc=_ENGINE,
+    engine_config=_ENGINE_CONFIG(
+        model="lmms-lab/llava-onevision-qwen2-7b-ov",
+        chat_template_builtin_name="chatml-llava",
+    ),
+    serving_config=_llava_model_serving_configs_7b,
+)
+
+
+# https://huggingface.co/lmms-lab/llava-onevision-qwen2-72b-ov
+LLAVA_ONEVISION_QWEN_2_72B_CONFIG = EzDeployConfig(
+    name="llava_onevision_qwen_2_72b_based",
+    engine_proc=_ENGINE,
+    engine_config=_ENGINE_CONFIG(
+        model="lmms-lab/llava-onevision-qwen2-72b-ov",
+        chat_template_builtin_name="chatml-llava",
+    ),
+    serving_config=_llava_model_serving_configs_70b,
+)
+
+
 @dataclass(frozen=True)
 class SglangVision:
-    LLAVA_1_5_LLAMA: EzDeployConfig = field(
-        default_factory=lambda: LLAVA_1_5_LLAMA_CONFIG
-    )
-    LLAVA_1_6_VICUNA: EzDeployConfig = field(
-        default_factory=lambda: LLAVA_1_6_VICUNA_CONFIG
-    )
-    LLAVA_1_6_YI_34B: EzDeployConfig = field(
-        default_factory=lambda: LLAVA_1_6_YI_34B_CONFIG
-    )
     LLAVA_NEXT_LLAMA3_8B: EzDeployConfig = field(
         default_factory=lambda: LLAVA_NEXT_LLAMA3_8B_CONFIG
+    )
+
+    LLAVA_NEXT_QWEN_1_5_72B_CONFIG: EzDeployConfig = field(
+        default_factory=lambda: LLAVA_NEXT_QWEN_1_5_72B_CONFIG
+    )
+
+    LLAVA_ONEVISION_QWEN_2_7B_CONFIG: EzDeployConfig = field(
+        default_factory=lambda: LLAVA_ONEVISION_QWEN_2_7B_CONFIG
+    )
+
+    LLAVA_ONEVISION_QWEN_2_72B_CONFIG: EzDeployConfig = field(
+        default_factory=lambda: LLAVA_ONEVISION_QWEN_2_72B_CONFIG
     )
---
+++ tests/integration/sglang/sanity-ezdeploy-A100.py
@@ -0,0 +1,84 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # These are sanity tests for ezdeploy for vLLM on A100
+
+# COMMAND ----------
+
+# MAGIC %pip install sglang[all]==0.3.0 outlines==0.0.44 httpx filelock
+# MAGIC %pip install -U flashinfer==0.1.6 --extra-index-url https://flashinfer.ai/whl/cu121/torch2.4/
+# MAGIC %pip install -U openai
+# MAGIC %pip install -U mlflow
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %pip list | grep sglang
+
+# COMMAND ----------
+
+# MAGIC %pip list | grep flashinfer
+
+# COMMAND ----------
+
+# MAGIC %pip list | grep vllm
+
+# COMMAND ----------
+
+from mlflow_extensions.databricks.deploy.gpu_configs import AzureServingGPUConfig
+from mlflow_extensions.testing.runner import ServerFramework, run_all_tests, RequestResult
+THIS_GPU = AzureServingGPUConfig.GPU_LARGE.value
+THIS_FRAMEWORK = ServerFramework.SGLANG
+THIS_GPU, THIS_FRAMEWORK
+
+# COMMAND ----------
+
+import os
+os.environ["HF_TOKEN"] = dbutils.secrets.get(scope="sri-mlflow-extensions", key="hf-token")
+
+# COMMAND ----------
+
+results = run_all_tests(gpu_config=THIS_GPU, server_framework=THIS_FRAMEWORK)
+
+# COMMAND ----------
+
+import pandas as pd
+display(pd.DataFrame(RequestResult.make_df_friendly(results)))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # FAILING TESTS
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+errored_results = [result for result in results if result.is_error is True]
+if len(errored_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(errored_results)))
+else:
+    print("No tests failed...")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Success Records
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+success_results = [result for result in results if result.is_error is False]
+if len(success_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(success_results)))
+else:
+    print("No tests passed...")
+
+# COMMAND ----------
+
+assert len(errored_results) == 0, "Tests failed"
+
+# COMMAND ----------
+
+
