f2ec12236fad67aedd01962f034a3515726161d1
stikkireddy <stikkireddy>
2024-09-06 00:05:52 +0000

    added sanity tests for vllm 0.6.0 on A100

---
+++ tests/integration/vllm/sanity-ezdeploy-A100.py
@@ -0,0 +1,70 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # These are sanity tests for ezdeploy for vLLM on A100
+
+# COMMAND ----------
+
+# MAGIC %pip install vllm==0.6.0 filelock httpx
+# MAGIC %pip install -U openai
+# MAGIC %pip install -U mlflow
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %pip list | grep vllm
+
+# COMMAND ----------
+
+from mlflow_extensions.databricks.deploy.gpu_configs import AzureServingGPUConfig
+from mlflow_extensions.testing.runner import ServerFramework, run_all_tests, RequestResult
+THIS_GPU = AzureServingGPUConfig.GPU_LARGE.value
+THIS_FRAMEWORK = ServerFramework.VLLM
+
+# COMMAND ----------
+
+import os
+os.environ["HF_TOKEN"] = dbutils.secrets.get(scope="sri-mlflow-extensions", key="hf-token")
+
+# COMMAND ----------
+
+results = run_all_tests(gpu_config=THIS_GPU, server_framework=THIS_FRAMEWORK)
+
+# COMMAND ----------
+
+import pandas as pd
+display(pd.DataFrame(RequestResult.make_df_friendly(results)))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # FAILING TESTS
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+errored_results = [result for result in results if result.is_error is True]
+if len(errored_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(errored_results)))
+else:
+    print("No tests failed...")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Success Records
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+success_results = [result for result in results if result.is_error is False]
+if len(success_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(success_results)))
+else:
+    print("No tests passed...")
+
+# COMMAND ----------
+
+
