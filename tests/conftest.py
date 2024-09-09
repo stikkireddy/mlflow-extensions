d89326e02ad109f8138aebc029e83b8fbc6d417e
natefleming-db <157608612+natefleming-db@users.noreply.github.com>
2024-09-09 16:51:41 -0400

    remove pyspark dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,6 @@ from databricks.sdk import WorkspaceClient
 from dotenv import find_dotenv, load_dotenv
 from mlflow import MlflowClient
 
-os.environ["PYSPARK_PYTHON"] = sys.executable
-os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
-os.environ["PYARROW_IGNORE_TIMEZONE"] = str(1)
-os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
 src_dir: Path = Path(__file__).parents[1]
 sys.path.insert(0, str(src_dir.resolve()))
