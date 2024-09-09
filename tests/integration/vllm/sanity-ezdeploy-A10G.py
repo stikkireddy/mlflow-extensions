69a95e6b8b2aeb4aae50fa5ec8687d5e7c2ef463
stikkireddy <stikkireddy>
2024-09-09 21:49:42 +0000

    testing for 1xA10

---
+++ tests/integration/vllm/sanity-ezdeploy-A10G.py
@@ -0,0 +1,74 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # These are sanity tests for ezdeploy for vLLM on A100
+
+# COMMAND ----------
+
+# MAGIC %pip install vllm==0.6.0 filelock httpx hf_transfer
+# MAGIC %pip install -U openai
+# MAGIC %pip install -U mlflow
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %pip list | grep vllm
+
+# COMMAND ----------
+
+from mlflow_extensions.databricks.deploy.gpu_configs import AWSServingGPUConfig
+from mlflow_extensions.testing.runner import ServerFramework, run_all_tests, RequestResult
+THIS_GPU = AWSServingGPUConfig.GPU_MEDIUM.value
+THIS_FRAMEWORK = ServerFramework.VLLM
+
+# COMMAND ----------
+
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+# COMMAND ----------
+
+results = run_all_tests(gpu_config=THIS_GPU, server_framework=THIS_FRAMEWORK)
+
+# COMMAND ----------
+
+import pandas as pd
+display(pd.DataFrame(RequestResult.make_df_friendly(results)))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # FAILING TESTS
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+errored_results = [result for result in results if result.is_error is True]
+if len(errored_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(errored_results)))
+else:
+    print("No tests failed...")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Success Records
+
+# COMMAND ----------
+
+import pandas as pd
+# errored records
+success_results = [result for result in results if result.is_error is False]
+if len(success_results) > 0:
+    display(pd.DataFrame(RequestResult.make_df_friendly(success_results)))
+else:
+    print("No tests passed...")
+
+# COMMAND ----------
+
+assert len(errored_results) == 0, "Tests failed"
+
+# COMMAND ----------
+
+
