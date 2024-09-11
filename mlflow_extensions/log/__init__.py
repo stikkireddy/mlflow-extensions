b84ce3aa6c1ad40c37f08ba190734be3041b571a
natefleming-db <157608612+natefleming-db@users.noreply.github.com>
2024-09-11 11:04:55 -0400

    add ip, host and version columns

--- mlflow_extensions/log/__init__.py
+++ mlflow_extensions/log/__init__.py
@@ -1,8 +1,6 @@
 import inspect
-import linecache
 import logging
-import os
-import sys
+import socket
 from enum import IntEnum
 from types import FrameType
 from typing import Any, Callable, Dict, List, Optional
@@ -12,6 +10,7 @@ import structlog.stdlib
 from pythonjsonlogger import jsonlogger
 from structlog.stdlib import BoundLogger
 from structlog.types import EventDict, WrappedLogger
+from mlflow_extensions.version import get_mlflow_extensions_version
 
 Logger = BoundLogger
 
@@ -88,11 +87,35 @@ def initialize_logging(
         else:
             raise structlog.DropEvent
 
+    version: str = get_mlflow_extensions_version()
+    def add_library_version(
+        logger: Logger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        event_dict["version"] = version
+        return event_dict
+
+    hostname: str = socket.gethostname()
+    def add_hostname(
+        logger: Logger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        event_dict["host"] = hostname
+        return event_dict
+    
+    ip: str = socket.gethostbyname(hostname)
+    def add_ip_address(
+        logger: Logger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        event_dict["ip"] = socket.gethostbyname(socket.gethostname())
+        return event_dict
+    
     structlog.configure_once(
         processors=[
             filter_by_level,
             structlog.stdlib.add_log_level,
             structlog.stdlib.add_logger_name,
+            add_hostname,
+            add_ip_address,
+            add_library_version,
             structlog.stdlib.PositionalArgumentsFormatter(),
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
