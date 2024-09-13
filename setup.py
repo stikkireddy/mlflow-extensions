382ca338c7fa7259b058061ef17f7e37a0e6efe3
Sri Tikkireddy <sri.tikkireddy+github@databricks.com>
2024-09-13 08:02:43 -0400

    fix mlflow skinny to 2.16.0 to use the run info model versions

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    install_requires = fh.read().splitlines()
+
 setup(
     name="mlflow-extensions",
     author="Sri Tikkireddy",
